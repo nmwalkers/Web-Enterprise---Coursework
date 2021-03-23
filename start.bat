842f0b9d4bc056dbbdb8552135226c983886a91b
nmwalkers <calumwalker14@gmail.com>
2021-03-23 15:10:33 +0000

    removed

--- start.bat
+++
@@ -1,5 +0,0 @@
-SET PORT=3000
-JWT_SECRET="mysecret"
-SET MONGODB_URI="mongodb+srv://adminuser:IOZBOChTmhGMoq8x@fitbawpatter.psfbe.mongodb.net/users?retryWrites=true&w=majority"
-npm run-script development
-
